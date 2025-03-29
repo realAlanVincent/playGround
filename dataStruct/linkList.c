636b8b374f4a2917d68cb431be272b9f1f00af58
Alan Vincent <realalanvincent@gmail.com>
2025-03-29 19:37:15 +0530

    Update linkList.c

--- dataStruct/linkList.c
+++ dataStruct/linkList.c
@@ -8,28 +8,68 @@ typedef struct Node {
 
 Node *head = NULL;
 
-void insert(int data, int pos) {
+void insert_begin(int data) {
+    Node *new_node = malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_end(int data) {
     Node *new_node = malloc(sizeof(Node)), *temp = head;
     new_node->data = data;
-    if (pos == 0 || !head) {
-        new_node->next = head;
+    new_node->next = NULL;
+    if (!head) {
         head = new_node;
         return;
     }
-    for (int i = 0; temp && i < pos - 1; i++)
+    while (temp->next)
         temp = temp->next;
-    new_node->next = temp ? temp->next : NULL;
-    if (temp) temp->next = new_node;
+    temp->next = new_node;
 }
 
-void delete(int pos) {
-    if (!head) return;
-    Node *temp = head, *prev = NULL;
+void insert_pos(int data, int pos) {
     if (pos == 0) {
-        head = head->next;
-        free(temp);
+        insert_begin(data);
         return;
     }
+    Node *new_node = malloc(sizeof(Node)), *temp = head;
+    new_node->data = data;
+    for (int i = 0; temp && i < pos - 1; i++)
+        temp = temp->next;
+    if (!temp) return;
+    new_node->next = temp->next;
+    temp->next = new_node;
+}
+
+void delete_begin() {
+    if (!head) return;
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void delete_end() {
+    if (!head) return;
+    if (!head->next) {
+        free(head);
+        head = NULL;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next->next)
+        temp = temp->next;
+    free(temp->next);
+    temp->next = NULL;
+}
+
+void delete_pos(int pos) {
+    if (!head) return;
+    if (pos == 0) {
+        delete_begin();
+        return;
+    }
+    Node *temp = head, *prev = NULL;
     for (int i = 0; temp && i < pos; i++) {
         prev = temp;
         temp = temp->next;
@@ -48,15 +88,25 @@ void print() {
 int main() {
     int choice, data, pos;
     while (1) {
-        printf("1. Insert\n2. Delete\n3. Print\n4. Exit\n");
+        printf("1. Insert at Beginning\n2. Insert at End\n3. Insert at Position\n4. Delete from Beginning\n5. Delete from End\n6. Delete from Position\n7. Print\n8. Exit\n");
         scanf("%d", &choice);
         if (choice == 1) {
-            scanf("%d %d", &data, &pos);
-            insert(data, pos);
+            scanf("%d", &data);
+            insert_begin(data);
         } else if (choice == 2) {
-            scanf("%d", &pos);
-            delete(pos);
+            scanf("%d", &data);
+            insert_end(data);
         } else if (choice == 3) {
+            scanf("%d %d", &data, &pos);
+            insert_pos(data, pos);
+        } else if (choice == 4) {
+            delete_begin();
+        } else if (choice == 5) {
+            delete_end();
+        } else if (choice == 6) {
+            scanf("%d", &pos);
+            delete_pos(pos);
+        } else if (choice == 7) {
             print();
         } else {
             break;
