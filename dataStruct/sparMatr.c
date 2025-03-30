ce35db6703bb6432f4d6f234cd6652aa7baa1c0e
Alan Vincent <realalanvincent@gmail.com>
2025-03-31 05:18:32 +0530

    Update sparMatr.c

--- dataStruct/sparMatr.c
+++ dataStruct/sparMatr.c
@@ -1,63 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#define MAX 10 // Maximum matrix size for simplicity
 
 typedef struct {
     int row, col, val;
 } Tuple;
 
-Tuple *create_sparse(int **matrix, int m, int n, int *size) {
+void create_sparse(int matrix[MAX][MAX], int rows, int cols, Tuple sparse[MAX * MAX], int *size) {
     int count = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] != 0) count++;
-        }
-    }
-    *size = count + 1;
-    Tuple *sparse = (Tuple *)malloc((*size) * sizeof(Tuple));
-    sparse[0].row = m;
-    sparse[0].col = n;
-    sparse[0].val = count;
-    int k = 1;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             if (matrix[i][j] != 0) {
-                sparse[k].row = i;
-                sparse[k].col = j;
-                sparse[k].val = matrix[i][j];
-                k++;
+                sparse[count + 1].row = i;
+                sparse[count + 1].col = j;
+                sparse[count + 1].val = matrix[i][j];
+                count++;
             }
         }
     }
-    return sparse;
+    sparse[0].row = rows;
+    sparse[0].col = cols;
+    sparse[0].val = count;
+    *size = count + 1;
+}
+
+void print_matrix(int matrix[MAX][MAX], int rows, int cols) {
+    printf("\nOriginal Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_sparse(Tuple sparse[MAX * MAX], int size) {
+    printf("\nSparse Matrix (Row, Column, Value):\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d  %d  %d\n", sparse[i].row, sparse[i].col, sparse[i].val);
+    }
 }
 
 int main() {
-    int m, n;
-    printf("Enter matrix dimensions (rows cols): ");
-    scanf("%d %d", &m, &n);
-
-    int **matrix = (int **)malloc(m * sizeof(int *));
-    for (int i = 0; i < m; i++) {
-        matrix[i] = (int *)malloc(n * sizeof(int));
-    }
-
-    printf("Enter matrix elements:\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    int rows, cols;
+    int matrix[MAX][MAX];
+    Tuple sparse[MAX * MAX];
+    
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    
+    printf("\nEnter the matrix elements (use 0 for sparse positions):\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
         }
     }
-
+    
+    print_matrix(matrix, rows, cols);
+    
     int size;
-    Tuple *sparse = create_sparse(matrix, m, n, &size);
-
-    printf("\nSparse Matrix Representation (3-Tuple Form):\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d %d %d\n", sparse[i].row, sparse[i].col, sparse[i].val);
-    }
-
-    free(sparse);
-    for (int i = 0; i < m; i++) free(matrix[i]);
-    free(matrix);
+    create_sparse(matrix, rows, cols, sparse, &size);
+    print_sparse(sparse, size);
+    
     return 0;
 }
